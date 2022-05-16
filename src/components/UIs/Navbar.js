b3b692d8ed3362a1d48475345f8c2da8a292fb6f
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-05-16 14:36:47 +0300

    navbars ready to go

--- src/components/UIs/Navbar.js
+++ src/components/UIs/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import css from './navbar.module.css';
 import logo from '../assets/images/meLogo.png';
+
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
     <div className={css.header}>
@@ -9,9 +12,9 @@ function Navbar() {
         </div> 
         <nav className={css.nav}>
                 <ul className={css.ul}>
-                    <li className={css.li}>Home</li>
-                    <li>Browse</li>
-                    <li>About</li>
+                    <li className={css.li}> <Link to='/'> Home </Link></li>
+                    <li className={css.li}> <Link to='/browse'> Browse </Link></li>
+                    <li className={css.li}> <Link to='/About'> About </Link></li>
                     <li> <a href="https://www.bc.fi/?gclid=Cj0KCQjwmuiTBhDoARIsAPiv6L957izCclO7mHzuc9Q4NksNezhLDQ4OZ9Xqjn5T-regQ-2zJDkA69UaAlLQEALw_wcB" target="blank">HBC</a> </li>
                 </ul>
         </nav>
