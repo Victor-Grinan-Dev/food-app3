ec61f77d81ae1767ddbec7326a574189cb5e43c3
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-05-16 15:04:38 +0300

    created the database

--- src/components/UIs/Navbar.js
+++ src/components/UIs/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import css from './navbar.module.css';
+import '../../App.css';
 import logo from '../assets/images/meLogo.png';
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className={css.header}>
         <div className={css.logo}>
-            <img src={logo}/>
+           <Link Link to='/'><img src={logo}/></Link> 
         </div> 
         <nav className={css.nav}>
                 <ul className={css.ul}>
-                    <li className={css.li}> <Link to='/'> Home </Link></li>
+                    <li className={css.li}> <Link to='/' className={({isActive}) => (isActive ? "active" : "")}> Home </Link></li>
                     <li className={css.li}> <Link to='/browse'> Browse </Link></li>
                     <li className={css.li}> <Link to='/About'> About </Link></li>
                     <li> <a href="https://www.bc.fi/?gclid=Cj0KCQjwmuiTBhDoARIsAPiv6L957izCclO7mHzuc9Q4NksNezhLDQ4OZ9Xqjn5T-regQ-2zJDkA69UaAlLQEALw_wcB" target="blank">HBC</a> </li>
