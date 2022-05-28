f5b11195e154d79d074952721e8b40c5ef535ae4
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-05-28 19:37:30 +0300

    single page kinda ready

--- src/components/pages/FoodSingle.js
+++ src/components/pages/FoodSingle.js
@@ -6,13 +6,22 @@ const FoodSingle = () => {
   const location = useLocation();
   const recipe = location.state.data;
   const country = location.state.country;
+  const ingrediets = location.state.ingrediets
 
   return (
-    <div >
-      <h1 className={css.foodName}>{recipe.name}</h1>
-      <img src={recipe.image} alt={recipe.name} className={css.image} />
-      <img src={country.flag} alt={country.name}  className={css.flag} />
-      <p>{recipe.instruction}</p>
+    <div className={css.singleFood}>
+      <div className={css.imageContainer}>
+        <img src={recipe.image} alt={recipe.name} className={css.image} />
+      </div>
+      
+      <div className={css.dataContainer}>
+        <div className={css.dataHeader}>
+          <h2 className={css.foodName}>"{recipe.name}"</h2>
+          <img src={country.flag} alt={country.name}  className={css.flag} />
+        </div>
+        
+        <p>{recipe.instruction}</p>
+      </div>
     </div>
   );
 };
